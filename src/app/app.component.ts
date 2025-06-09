9b612b9a3756e19ada5b5e36471681a671f4273d
eric <eric560065@gmail.com>
2025-06-09 13:16:22 +0800

    feat(product): 依產品清單顯示實作

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { ProductCardListComponent } from './product-card-list/product-card-list.component';
+import { Product } from './models/product';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,42 @@ import { ProductCardListComponent } from './product-card-list/product-card-list.
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  products = [
+    new Product({
+      name: 'A 產品',
+      authors: '作者A、作者B、作者C',
+      company: '博碩文化',
+      isShow: true,
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+    }),
+    new Product({
+      name: 'B 產品',
+      authors: '作者A、作者B、作者C',
+      company: '博碩文化',
+      isShow: true,
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+    }),
+    new Product({
+      name: 'C 產品',
+      authors: '作者A、作者B、作者C',
+      company: '博碩文化',
+      isShow: true,
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+    }),
+    new Product({
+      name: 'D 產品',
+      authors: '作者A、作者B、作者C',
+      company: '博碩文化',
+      isShow: true,
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+    }),
+    new Product({
+      name: 'E 產品',
+      authors: '作者A、作者B、作者C',
+      company: '博碩文化',
+      isShow: true,
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+    }),
+  ];
+}
--- src/app/product-card-list/product-card-list.component.ts
+++ src/app/product-card-list/product-card-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ProductCardComponent } from '../product-card/product-card.component';
 import { Product } from '../models/product';
 
@@ -9,14 +9,6 @@ import { Product } from '../models/product';
   styleUrl: './product-card-list.component.scss',
 })
 export class ProductCardListComponent {
-  product = new Product({
-    name: 'A 產品',
-    authors: '作者A、作者B、作者C',
-    company: '博碩文化',
-    isShow: true,
-    photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-  });
-  onSetDisplay(isShow: boolean): void {
-    this.product.isShow = isShow;
-  }
+  @Input()
+  products!: Product[];
 }
